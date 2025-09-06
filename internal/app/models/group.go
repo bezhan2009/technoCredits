246732b7f4197daa3e08cfb0f743502e81a4e3b8
Ilhomiddin123 <ilhomiddinrakhimov@gmail.com>
2025-09-06 13:54:02 +0500

    Create  CRUD Groups, GroupsMembers

--- internal/app/models/group.go
+++ internal/app/models/group.go
@@ -2,7 +2,6 @@ package models
 
 import "time"
 
-// Group представляет таблицу groups.
 type Group struct {
 	ID        uint   `gorm:"primaryKey"`
 	Name      string `gorm:"type:varchar(255);not null"`
@@ -11,7 +10,6 @@ type Group struct {
 	CreatedAt time.Time
 }
 
-// GroupMember представляет таблицу groups_member, которая является связующей таблицей для отношений многие ко многим.
 type GroupMember struct {
 	GroupID  uint      `gorm:"primaryKey"`
 	UserID   uint      `gorm:"primaryKey"`
