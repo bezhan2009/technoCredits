e9f864c7cf4a2ff970695a2e08a3aa3ecc21055c
bezhan2009 <karimovbezan0@gmail.com>
2025-09-06 16:02:45 +0300

    added crud for users

--- internal/controllers/users.go
+++ internal/controllers/users.go
@@ -9,6 +9,37 @@ import (
 	"technoCredits/pkg/errs"
 )
 
+func GetMyDataUser(c *gin.Context) {
+	userID := c.GetUint(middlewares.UserIDCtx)
+
+	user, err := service.GetUserByID(userID)
+	if err != nil {
+		HandleError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func UpdateUser(c *gin.Context) {
+	var user models.User
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		HandleError(c, errs.ErrValidationFailed)
+		return
+	}
+
+	user.ID = c.GetUint(middlewares.UserIDCtx)
+
+	err = service.UpdateUser(user)
+	if err != nil {
+		HandleError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Worker updated successfully"})
+}
+
 func UpdateUsersPassword(c *gin.Context) {
 	userID := c.GetUint(middlewares.UserIDCtx)
 
