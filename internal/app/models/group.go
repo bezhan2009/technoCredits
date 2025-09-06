5156fb76e5a085a89dd2adc89c4c5bac7a71e992
bezhan2009 <karimovbezan0@gmail.com>
2025-09-06 14:04:03 +0300

    fixed groups logic and added role to groups

--- internal/app/models/group.go
+++ internal/app/models/group.go
@@ -4,19 +4,23 @@ import "time"
 
 type Group struct {
 	ID        uint   `gorm:"primaryKey"`
-	Name      string `gorm:"type:varchar(255);not null"`
-	OwnerID   uint   `gorm:"not null"`
-	Owner     User   `gorm:"foreignKey:OwnerID"`
+	Name      string `json:"name" gorm:"type:varchar(255);not null"`
+	OwnerID   uint   `json:"owner_id" gorm:"not null"`
+	Owner     User   `json:"-" gorm:"foreignKey:OwnerID"`
 	CreatedAt time.Time
 
 	GroupMembers []GroupMember `gorm:"foreignKey:GroupID"`
 }
 
 type GroupMember struct {
-	GroupID  uint      `gorm:"primaryKey;autoIncrement:false"`
-	UserID   uint      `gorm:"primaryKey;autoIncrement:false"`
-	Group    Group     `gorm:"foreignKey:GroupID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	User     User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Role     string    `gorm:"type:varchar(255)"`
+	ID      uint  `gorm:"primaryKey"`
+	GroupID uint  `gorm:"primaryKey;autoIncrement:false"`
+	UserID  uint  `gorm:"primaryKey;autoIncrement:false"`
+	Group   Group `json:"-" gorm:"foreignKey:GroupID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	User    User  `json:"-" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+
+	RoleID uint `gorm:"not null"`
+	Role   Role `json:"-" gorm:"foreignKey:RoleID"`
+
 	JoinedAt time.Time `gorm:"not null"`
 }
